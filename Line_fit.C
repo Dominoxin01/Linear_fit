86e78307a0bc803be4218c2e695fcb92e14746c2
Dominik Stajkowski <d.stajkowski.1@pgr.bham.ac.uk>
2023-12-29 15:15:13 +0000

    Added a second pannel which shows the computed residuals.

--- Line_fit.C
+++ Line_fit.C
@@ -15,17 +15,46 @@ void LinFit(const char *filename){
 	};
 	
 	TCanvas *c1 = new TCanvas("c1", "hat", 200, 10, 700, 500);
-	c1-> SetGrid();
+	TPad *pad1 = new TPad("pad1", "plot", 0, 0.33, 1, 1);
+	pad1->SetGrid();
+	pad1->Draw();
 	
+	TPad *pad2 = new TPad("pad2", "Residuals", 0, 0, 1, 0.33);
+	pad2->SetGrid();
+	pad2->Draw();
+	
+	pad1->cd();
 	TGraphErrors *gr = new TGraphErrors(filename);
 	gr->SetTitle("TGraphErrors Example");
 	gr->SetMarkerColor(4);
 	gr->SetMarkerStyle(21);
 	gr->Draw("AP");
-	
 	gr->Fit("pol1", "F");
+	
+	pad2->cd();
+	TF1 *g = (TF1*) gr->GetListOfFunctions()->FindObject("pol1");
+	double c = g->GetParameter(0);
+	double m = g->GetParameter(1);
+	TF1 *f = new TF1("lin", "pol1");
+	f->FixParameter(0, c);
+	f->FixParameter(1,m);
+	
+	int n_data = gr->GetN();
 
-	//gr->Draw("c1");
+	
+	TGraphErrors *res = new TGraphErrors();
+	for (int  i = 0; i < n_data; i++){
+		double y_res = gr->GetPointY(i) - f->Eval(gr->GetPointX(i));
+		
+		res->SetPoint(i, gr->GetPointX(i), y_res);
+	} 
+	res->SetMarkerColor(4);
+	res->SetMarkerStyle(21);
+	
+	res->Draw("AP");
+
 }
+
+
 
 
